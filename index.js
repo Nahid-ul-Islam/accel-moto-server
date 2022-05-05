90ab4f3f61b125f84f1375859b440330f10f8c46
Nahid-ul-Islam <nahidul522@gmail.com>
2022-05-06 03:10:38 +0600

    items loaded to ui

--- index.js
+++ index.js
@@ -11,14 +11,34 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6myik.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6myik.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("moto").collection("bikes");
-  console.log("db connected");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const bikesCollection = client.db("moto").collection("bikes");
+        //get items
+        //http://localhost:5000/bikes
+        app.get('/bikes', async (req, res) => {
+            const query = {};
+            const cursor = bikesCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        //add item
+        app.post('/bikes', async(req, res) => {
+            const newItem = req.body;
+            const result = await bikesCollection.insertOne(newItem);
+            res.send(result);
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
