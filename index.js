d0c0294f606b5ed28f5f327627ea1c143b6c8900
Nahid-ul-Islam <nahidul522@gmail.com>
2022-05-08 02:08:51 +0600

    JWT done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const ObjectId = require('mongodb').ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const verify = require('jsonwebtoken/verify');
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +11,22 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'FORBIDDEN ACCESS' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
@@ -31,6 +49,16 @@ async function run() {
         console.log('db connected');
 
 
+
+        //AUTH
+        app.post('/gettoken', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '5d'
+            })
+            res.send({ accessToken });
+        })
+
         //get items
         //http://localhost:5000/bikes
         app.get('/bikes', async (req, res) => {
@@ -44,60 +72,67 @@ async function run() {
         //get single item
         app.get('/bikes/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await bikesCollection.findOne(query);
             res.send(result);
         });
 
 
         //update item
-         app.put('/bikes/:id', async(req, res) => {
-             const id = req.params.id;
-             //console.log("id", id);
-             const updateItem = req.body;
-             //console.log(updateItem)
-             const filter = {_id: ObjectId(id)};
-             const options = { upsert: true };
-             const updateDoc = {
-                 $set: {
-                     quantity : updateItem.quantity
-                 }
-             };
-             const result = await bikesCollection.updateOne(filter, updateDoc, options);
-             res.send(result);
-         });
-
-
-         //delte an item
-         app.delete('/bikes/:id', async(req, res) => {
+        app.put('/bikes/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}; 
+            //console.log("id", id);
+            const updateItem = req.body;
+            //console.log(updateItem)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateItem.quantity
+                }
+            };
+            const result = await bikesCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+
+        //delte an item
+        app.delete('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
             const result = await bikesCollection.deleteOne(query);
             res.send(result);
         });
 
 
         //add a new item
-        app.post('/bikes', async(req, res) => {
+        app.post('/bikes', async (req, res) => {
             const newItem = req.body;
             console.log('adding new item', newItem);
             const result = await bikesCollection.insertOne(newItem);
             res.send(result);
-         });
+        });
 
 
-         //search query by email
-         app.get('/my-bikes', async (req, res) => {
+        //search query by email
+        app.get('/my-bikes', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = {email:email};
-            const curosr = bikesCollection.find(query);
-            const result = await curosr.toArray();
-            res.send(result);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const curosr = bikesCollection.find(query);
+                const result = await curosr.toArray();
+                res.send(result);
+            }
+            else{
+                res.status(403).send({message: 'FORBIDDEN ACCESS'})
+            }
+
         });
     }
 
 
-        
+
     finally {
         // await client.close();
     }
